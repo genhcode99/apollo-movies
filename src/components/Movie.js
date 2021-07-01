a1933340d8e9aef94bed183e888f007db60f8135
JeUpark <genhcode99@gmail.com>
2021-07-01 10:17:45 +0900

    #1.3 Apollo Cache and Styles

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,11 +1,29 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import styled from 'styled-components'
 
-const Movie = ({ id }) => {
+const Container = styled.div`
+  height: 380px;
+  width: 100%;
+`
+
+const Poster = styled.div`
+  background-image: url(${(props) => props.bg});
+  height: 100%;
+  width: 100%;
+  background-size: cover;
+  background-position: center;
+  box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23);
+  border-radius: 7px;
+`
+
+const Movie = ({ id, bg }) => {
   return (
-    <div>
-      <Link to={`/${id}`}>{id}</Link>
-    </div>
+    <Container>
+      <Link to={`/${id}`}>
+        <Poster bg={bg} />
+      </Link>
+    </Container>
   )
 }
 export default Movie
